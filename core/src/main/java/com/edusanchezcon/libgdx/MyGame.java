cb73b32c43d3142b89906736ef1a9c58cce9156a
Eduardo Sanchez <esanchez@autentia.com>
2020-05-28 15:31:09 +0200

    Added atlas for the "runner punk" animation

--- core/src/main/java/com/edusanchezcon/libgdx/MyGame.java
+++ core/src/main/java/com/edusanchezcon/libgdx/MyGame.java
@@ -2,13 +2,18 @@ package com.edusanchezcon.libgdx;
 
 import com.badlogic.gdx.Game;
 import com.edusanchezcon.libgdx.screens.Screen1;
+import com.edusanchezcon.libgdx.screens.Screen2;
+import com.edusanchezcon.libgdx.screens.Screen3;
+import com.edusanchezcon.libgdx.screens.Screen4;
 import com.edusanchezcon.libgdx.screens.Screen5;
+import com.edusanchezcon.libgdx.screens.Screen6;
+import com.edusanchezcon.libgdx.screens.Screen7;
 import com.edusanchezcon.libgdx.screens.Screen9;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MyGame extends Game {
 	@Override
 	public void create() {
-		setScreen(new Screen1());
+		setScreen(new Screen9());
 	}
 }
